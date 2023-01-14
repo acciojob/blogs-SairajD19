6ff35ff52712170ef4278d17da07159db318bba4
SairajD19 <sairajdawate25@gmail.com>
2023-01-14 21:37:35 +0530

    eight try

--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -6,6 +6,8 @@ import io.swagger.models.auth.In;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
 public class ImageService {
     @Autowired
@@ -30,7 +32,7 @@ public class ImageService {
     public int countImagesInScreen(Image image, String screenDimensions) {
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
         //In case the image is null, return 0
-        try{
+        /*try{
             Image image1 = imageRepository2.findById(image.getId()).get();
             String [] arr = image1.getDimensions().split("X");
             int total = Integer.parseInt(arr[0]) * Integer.parseInt(arr[1]);
@@ -42,7 +44,14 @@ public class ImageService {
         }catch (Exception e) {
             return 0;
         }
-        return 0;
+        return 0;*/
+        if(screenDimensions.split("X").length==2 || Objects.nonNull(image)){
+            Integer maxLength= Integer.parseInt(screenDimensions.split("X")[0])/Integer.parseInt(image.getDimensions().split("X")[0]);
+            Integer maxWidth= Integer.parseInt(screenDimensions.split("X")[1])/Integer.parseInt(image.getDimensions().split("X")[1]);
+
+            return  maxWidth*maxLength;
+        }
+        return  0;
 
     }
 }
